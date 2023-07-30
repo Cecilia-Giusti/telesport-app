18a6c0c49c7b96ccdc5d10ab800a5fd9764f994d
Cecilia Giusti <cecilia@tete-chercheuse.fr>
2023-07-30 08:43:47 +0200

    Feat: add number of jos and number of countries

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,7 +5,8 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { Router } from '@angular/router';
 import { ErrorService } from 'src/app/core/services/error.service';
-import { single } from '../../../assets/single';
+import { map } from 'rxjs/operators';
+import { Country } from 'src/app/core/models/Country.model';
 
 @Component({
   selector: 'app-home',
@@ -16,8 +17,10 @@ export class HomeComponent implements OnInit, OnDestroy {
   public olympics$: Observable<Olympic[]> = of([]);
   private unsubscribe$ = new Subject<void>();
   public errorStatus$: Observable<number | null> = of();
+  public countries$: Observable<Country[]> = of([]);
+  public countryCount$: Observable<number> = of(0);
+  public yearCount$: Observable<number> = of(0);
 
-  single: any;
   view: [number, number] = [700, 400];
 
   // options
@@ -25,22 +28,42 @@ export class HomeComponent implements OnInit, OnDestroy {
   istooltipDisabled: boolean = true;
   showLabels: boolean = true;
   isDoughnut: boolean = false;
-  legendPosition: any = 'right';
-
-  colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
-  };
 
   constructor(
     private olympicService: OlympicService,
     private router: Router,
     private errorService: ErrorService
   ) {
-    Object.assign(this, { single });
+    Object.assign(this);
   }
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    this.countries$ = this.olympicService.getOlympics().pipe(
+      map((olympics: Olympic[]) =>
+        olympics.map((olympic: Olympic) => {
+          let totalMedals = olympic.participations.reduce(
+            (sum, participation) => sum + participation.medalsCount,
+            0
+          );
+          return new Country(olympic.country, totalMedals);
+        })
+      )
+    );
+
+    this.countryCount$ = this.olympicService
+      .getOlympics()
+      .pipe(map((olympics: Olympic[]) => olympics.length));
+
+    this.yearCount$ = this.olympicService.getOlympics().pipe(
+      map((olympics: Olympic[]) => {
+        const allYears = olympics.flatMap((olympic) =>
+          olympic.participations.map((participation) => participation.year)
+        );
+        const uniqueYears = Array.from(new Set(allYears));
+        return uniqueYears.length;
+      })
+    );
+
     this.olympics$.pipe(takeUntil(this.unsubscribe$)).subscribe();
 
     this.errorService.errorStatus$
